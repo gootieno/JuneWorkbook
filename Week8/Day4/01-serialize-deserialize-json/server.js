4fb86d102b90cf1ea24b6290f9e9bc5ac547c58e
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-08-03 09:26:49 -0700

    serialize json solution

--- Week8/Day4/01-serialize-deserialize-json/server.js
+++ Week8/Day4/01-serialize-deserialize-json/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
@@ -10,37 +10,43 @@ const server = http.createServer((req, res) => {
 
   req.on("end", () => {
     // Parse the body of the request as JSON if Content-Type header is
-      // application/json
+    // application/json
     // Parse the body of the request as x-www-form-urlencoded if Content-Type
-      // header is x-www-form-urlencoded
+    // header is x-www-form-urlencoded
     if (reqBody) {
-
       // check for content type and parse accordingly through conditional
-      req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-        .map(([key, value]) => [key, decodeURIComponent(value)])
-        .reduce((acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        }, {});
-
+      if (req.headers["content-type"] === "application/json") {
+        req.body = JSON.parse(reqBody);
+      } else {
+        req.body = reqBody
+          .split("&")
+          .map((keyValuePair) => keyValuePair.split("="))
+          .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+          .map(([key, value]) => [key, decodeURIComponent(value)])
+          .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          }, {});
+      }
       // Log the body of the request to the terminal
       console.log(req.body);
     }
 
-    const resBody = {
-      "Hello": "World!"
-    };
-
     // Return the `resBody` object as JSON in the body of the response
 
-
     // stringify response and res.end(response)
+    if (req.method === "GET" && req.url === "/") {
+      const resBody = {
+        Hello: "World!",
+      };
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      return res.end(JSON.stringify(resBody));
+    }
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
