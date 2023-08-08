9024a2af49ff948e0599e9e22e07d6f38c8570be
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-08-08 09:24:18 -0700

    nodelist / element selection solution

--- Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
+++ Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
@@ -1,11 +1,22 @@
 export default () => {
   const bodyChildElements = document.body.children; // HTMLCollection [div]
-  // console.log(bodyChildElements);
+  console.log("body child elements ", bodyChildElements);
 
   const div = bodyChildElements[0];
 
   const divChildElements = div.children; // HTMLCollection [span]
+  console.log("div children ", divChildElements);
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+  console.log("div inner text ", helloWorld);
   const span = divChildElements[0]; // <span>Yes!</span>
+
+  for (let i = 0; i < divChildElements.length; i++) {
+    const nextElement = document.createElement("span");
+    nextElement.innerText = "another element";
+    div.appendChild(nextElement);
+
+    if (i === 50) break;
+  }
+
   // debugger
 };
--- Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/node-list.js
+++ Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/node-list.js
@@ -1,11 +1,14 @@
 export default () => {
   const bodyChildNodes = document.body.childNodes; // NodeList [text, div, text]
-  // console.log(bodyChildNodes);
+  console.log("body child nodes ", bodyChildNodes);
 
   const div = bodyChildNodes[1]; // NOT bodyChildNodes[0]
 
   const divChildNodes = div.childNodes; // NodeList [text, span, text]
   const helloWorld = divChildNodes[0].textContent; // Hello World!\n
+
+  console.log("div child node text content ", helloWorld);
+  console.log('div inner text ', divChildNodes[1].innerText)
   const span = divChildNodes[1]; // <span>Yes!</span>
   // debugger
 };
