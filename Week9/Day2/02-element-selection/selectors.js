02e12fb583a3c1ef9df3924baa001a206719ec8f
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-08-08 11:02:41 -0700

    create remove elements

--- Week9/Day2/02-element-selection/selectors.js
+++ Week9/Day2/02-element-selection/selectors.js
@@ -1,38 +1,65 @@
 const select = () => {
-    /* Write queries for each of the following */
+  /* Write queries for each of the following */
 
-    /* Section 1 */
-    // 1. Get all seeded fruit elements
-    // Your code here
-    
-    // 2. Get all seedless fruit elements
-    // Your code here
+  /* Section 1 */
+  // 1. Get all seeded fruit elements
+  // Your code here
+  const seeded = document.getElementsByClassName("seed");
+  console.log("seeded ", seeded);
+  // 2. Get all seedless fruit elements
+  // Your code here
+  const seedless = document.getElementsByClassName("seedless");
+  console.log("seedless ", seedless);
+  // 3. Get first seedless fruit element
+  // Your code here
+  const firstSeedless = document.querySelector(".seedless");
+  console.log("first seedless ", firstSeedless);
+  /* Section 2 */
+  // 4. Get inner span with text "you"
+  // Your code here
+  const youDiv = document.getElementById("wrapper");
+  
+  youDiv.style.borderWidth = "1px";
+  youDiv.style.borderColor = "blue";
+  youDiv.style.borderStyle = "solid";
 
-    // 3. Get first seedless fruit element
-    // Your code here
+  const youSpan = youDiv.querySelector("span");
+  console.log("span text ", youSpan.innerText);
+  // 5. Get all children of element "wrapper"
+  // Your code here
+  const wrapperChildren = youDiv.children;
+  console.log("#wrapper children ", wrapperChildren);
+  // 6. Get all odd number list items in the list
+  // Your code here
+  const oddList = document.querySelectorAll("#two .odd");
+  console.log("odd list ", oddList);
+  // 7. Get all even number list items in the list
+  // Your code here
+  const evenNums = [...oddList].map((el) => el.nextElementSibling);
+  console.log("even nums ", evenNums);
+  /* Section 3 */
+  // 8. Get all tech companies without a class name
+  // Your code here
+  const sectionThree = document.getElementById("three");
 
-    /* Section 2 */
-    // 4. Get inner span with text "you"
-    // Your code here
+  const pTag = sectionThree.querySelector("p");
 
-    // 5. Get all children of element "wrapper"
-    // Your code here
+  const classLessTech = pTag.children[0];
+  console.log("classless tech ", classLessTech);
+  // 9. Get "Amazon" list element
+  // Your code here
+  const amazon = document.getElementsByClassName("shopping");
+  console.log("Amazon ", amazon);
+  // 10. Get all unicorn list elements (not the image element)
+  // Your code here
+  const unicorns = [...document.getElementsByClassName("unicorn")].map(
+    (el) => el.parentElement
+  );
 
-    // 6. Get all odd number list items in the list
-    // Your code here
+  console.log("unicorns ", unicorns);
 
-    // 7. Get all even number list items in the list
-    // Your code here
-
-    /* Section 3 */
-    // 8. Get all tech companies without a class name
-    // Your code here
-
-    // 9. Get "Amazon" list element
-    // Your code here
-
-    // 10. Get all unicorn list elements (not the image element)
-    // Your code here
-}
+  //SET ATTRIBUTE
+  document.body.setAttribute("class", "darkMode");
+};
 
 window.onload = select;
--- Week9/Day2/03-create-remove-elements/create-remove.js
+++ Week9/Day2/03-create-remove-elements/create-remove.js
@@ -1,44 +1,77 @@
 /****************************** ADD DOG BUTTON ******************************/
 const add = document.getElementById("add");
 add.addEventListener("click", async () => {
-    try {
-        const res = await fetch("https://dog.ceo/api/breeds/image/random")
-        const data = await res.json();
+  try {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
 
-        const url = data.message; // URL of new dog image
+    const url = data.message; // URL of new dog image
+    console.log("data ", data);
+    console.log("url from data ", url);
+    /*--------------- Get breed (Hint: Parse from URL) ---------------- */
+    // Your code here
 
-        /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here
+    // const section = document.querySelector("section");
 
-        /*------------ Create new dog card with the url above ------------- */
-        /* (use the HTML structure for the current dog image in the index.html
+    // const div = document.createElement("div");
+
+    // const button = document.createElement("button");
+
+    // button.innerText = "Click Me";
+
+    // div.appendChild(button);
+
+    // div.setAttribute("class", "section-child");
+
+    // section.appendChild(div);
+
+    /*
+    <ul>
+        <li>
+            <figure>
+                <img src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg" />
+                <figcaption>hound-afghan</figcaption>
+            </figure>
+        </li>
+    </ul>
+
+    1. create/select elements 
+      - create elements if needed
+      - select a live element 
+    
+    2. manipulation
+      set innerText/attributes on created elements
+
+    3. append all elements in order all the way to live element
+
+    */
+
+    /*------------ Create new dog card with the url above ------------- */
+    /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-        // Your code here
+    // Your code here
 
-        /* Add the new dog card as a child to the ul in the .gallery element */
-        // Your code here
-
-    } catch (e) {
-        console.log("Couldn't fetch dog :(")
-    }
+    /* Add the new dog card as a child to the ul in the .gallery element */
+    // Your code here
+  } catch (e) {
+    console.log("Couldn't fetch dog :(");
+  }
 });
 
 /************************** REMOVE FIRST DOG BUTTON **************************/
 const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
-    /*-------------------- Select the first dog card --------------------- */
-    // Your code here
-
-    /*-------------------- Remove the first dog card --------------------- */
-    // Your code here
+  /*-------------------- Select the first dog card --------------------- */
+  // Your code here
+  /*-------------------- Remove the first dog card --------------------- */
+  // Your code here
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
 const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
-    /*-------------------- Select the last dog card ----------------------- */
-    // Your code here
-
-    /*-------------------- Remove the last dog card ----------------------- */
-    // Your code here
+  /*-------------------- Select the last dog card ----------------------- */
+  // Your code here
+  /*-------------------- Remove the last dog card ----------------------- */
+  // Your code here
 });
